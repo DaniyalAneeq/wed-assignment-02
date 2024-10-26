ad9b8148adec855a3501b5f193df9294bdfda20e
Your Name <you@example.com>
2024-10-27 02:15:18 +0500

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily:{
+        Montserrat: ["Montserrat", "serif"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
